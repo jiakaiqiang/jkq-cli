7cfad004b2ae7757dd4cffc70724bf200ee7ab87
jiakaiqiang <13165792976@163.com>
2024-01-29 16:53:46 +0800

    ‘feat:初始化’

--- examples/app/node_modules/jkq-cli/bin/index.js
+++ examples/app/node_modules/jkq-cli/bin/index.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-const {inquirerPrompt} =  require('./inquirer/inquirer');
+const {inquirerPrompt} =  require('./inquirer');
 const yargs = require('yargs');
+const path   = require('path');
+//引入拷贝的的函数 
+
+const { copyDir, checkMkdirExists } = require("./copy");
 yargs.command(
     ['create','c'],
     '创建一个新的项目',
@@ -15,8 +19,21 @@ yargs.command(
     function(argv){
         console.log('创建项目：',argv)
         inquirerPrompt(argv).then((answer)=>{
-        console.log(answer)
-
+            const { name, type } = answer;
+            console.log('项目名称：',name)
+            const isMkdirExists = checkMkdirExists(
+              path.resolve(process.cwd(),`./src/pages/${name}`)
+            );
+            if (isMkdirExists) {
+              console.log(`${name}文件夹已经存在`)
+            } else {
+              copyDir(
+                path.resolve(__dirname, `./template/${type}`),  //
+                path.resolve(process.cwd(), `./src/pages/${name}`) //运行的地址 process.cwd() 绝对地址  最好使用的是绝对的地址
+              )
+            }
+      
+   
         })
     }
 ).argv;
